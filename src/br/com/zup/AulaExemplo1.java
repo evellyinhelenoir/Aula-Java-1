9c0248186c13ad60c762b7c1d72f3ab4281c6442
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-08-24 14:00:14 -0300

    Meu Primeiro Commit

---
+++ src/br/com/zup/AulaExemplo1.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class AulaExemplo1 {
+    public static void main(String[] args) {
+       int idade = 18;
+       String nome = "Marcio";
+       System.out.println(idade);
+        System.out.println(nome);
+    }
+}
