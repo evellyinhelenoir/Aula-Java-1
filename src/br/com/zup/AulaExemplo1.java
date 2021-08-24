4b02f7e2377f50c9683ab0ca5b5d5e2711902711
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-08-24 14:13:04 -0300

    Meu segundo commit

--- src/br/com/zup/AulaExemplo1.java
+++ src/br/com/zup/AulaExemplo1.java
@@ -2,9 +2,14 @@ package br.com.zup;
 
 public class AulaExemplo1 {
     public static void main(String[] args) {
+      // Print sem concatenar
        int idade = 18;
        String nome = "Marcio";
        System.out.println(idade);
-        System.out.println(nome);
+       System.out.println(nome);
+
+       // Print com concatenar
+
+        System.out.println("Olá meu nome é " + nome + " e tenho " + idade);
     }
 }
